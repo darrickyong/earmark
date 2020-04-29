a822643d1c85c3efb8e2e21a5c3fc3f1c6ba607c
darrickyong <darrickyong@gmail.com>
2020-04-29 13:52:43 -0700

    Added routes for payments/transactions/expenses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :show, :create]
+    resources :expenses, only: [:index, :show, :create, :update, :destroy]
+    resources :transactions, only: [:index, :show, :create, :update, :destroy]
+    resources :payments, only: [:index, :show, :create, :update, :destroy]
+  
     resource :session, only: [:create, :destroy]
   end
 
