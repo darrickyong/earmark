70263dedb3c17b59bbfa21c27a44e5f738083ddc
darrickyong <darrickyong@gmail.com>
2020-04-29 10:39:48 -0700

    Adding CSS media query for navbar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -4,36 +4,44 @@ class NavBar extends React.Component {
   render() {
     return (
       <header className="nav-bar">
-        <div className="nav-logo">Earmark Nav Bar</div>
-        
-        {this.props.currentUser ? (
-          <div>
-            {this.props.user.name.split(" ")[0]}
-            <button 
-              className="nav-logout-button"
-              onClick={this.props.logout}
-            >Logout
-            </button>
-          </div>
-        ) : (
-          <div>
-              <a
-                href="/#/login"
-                className="nav-login-button"
-              >
-                Log in
-            </a>
-            
-            or
 
-            <a 
-              href="/#/signup"
-              className="nav-signup-button"
-            >
-              Sign up
-            </a>
-          </div>
-        )}
+        <div className="nav-container">
+          {/* <div className="nav-logo">Earmark Nav Bar</div> */}
+          <a 
+            href="/#/"
+            className="nav-logo"
+          >Earmark Nav Bar</a>
+          
+          {this.props.currentUser ? (
+            <div>
+              {this.props.user.name.split(" ")[0]}
+              <button 
+                className="nav-logout-button"
+                onClick={this.props.logout}
+              >Logout
+              </button>
+            </div>
+          ) : (
+            <div>
+                <a
+                  href="/#/login"
+                  className="nav-login-button"
+                >
+                  Log in
+              </a>
+              
+              or
+
+              <a 
+                href="/#/signup"
+                className="nav-signup-button"
+              >
+                Sign up
+              </a>
+            </div>
+          )}
+        </div>
+        
         
 
 
