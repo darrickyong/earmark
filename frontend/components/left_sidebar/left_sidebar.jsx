9411aa2fb669a9a6dc35ae9f9ce05e0d8d46aa0b
darrickyong <darrickyong@gmail.com>
2020-05-03 00:02:36 -0700

    Adding styling to friends and left sidebar

--- frontend/components/left_sidebar/left_sidebar.jsx
+++ frontend/components/left_sidebar/left_sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPiggyBank, faDollarSign, faListUl } from '@fortawesome/free-solid-svg-icons';
 import FriendIndexContainer from "../friends/friend_index_container";
 
 class LeftSidebar extends React.Component {
@@ -7,10 +9,28 @@ class LeftSidebar extends React.Component {
     return (
       <div className="left-sidebar">
         <div className="left-sidebar-container">
-          Left Sidebar
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/payments">Payments</Link>
-          <Link to="/expenses">All expenses</Link>
+          <div className="left-sidebar-main">
+            <div className="left-dashboard">
+              <Link to="/dashboard" className="left-link">
+                <FontAwesomeIcon icon={faPiggyBank} className="left-pig"/>
+                Dashboard
+              </Link>
+            </div>
+            
+            <div className="left-payment">
+              <Link to="/payments" className="left-link">
+                <FontAwesomeIcon icon={faDollarSign} className="left-dollar" />
+                Payments</Link>
+            </div>
+
+            <div className="left-expense">
+              <Link to="/expenses" className="left-link">
+                <FontAwesomeIcon icon={faListUl} className="left-ul" />
+                All expenses
+              </Link>
+            </div>
+          </div>
+
           <FriendIndexContainer />
         </div>
       </div>
