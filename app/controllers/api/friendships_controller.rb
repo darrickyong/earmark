7af58006acff0fefe863c0de0bf81e2d1c80d000
darrickyong <darrickyong@gmail.com>
2020-05-04 13:54:38 -0700

    Fixed friendship creation refactor

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -3,12 +3,16 @@ class Api::FriendshipsController < ApplicationController
     @friendships = current_user.friendships + current_user.other_friendships
   end
 
+  def show
+    @friendship = Friendship.find_by(id: params[:id])
+  end
+
   def create
+    # debugger
     @user = User.find_by(email: friendship_params[:email])
     if @user
       @friendship = Friendship.create(user_id: current_user.id, friend_id: @user.id)      
       if @friendship.save
-        @friendship = @user
         render :show
       else
         render json: ["You are already friends."], status: 422
