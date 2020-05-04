120c47ade03a9f54c374df4cca1a35cfb4ee65a6
darrickyong <darrickyong@gmail.com>
2020-05-03 22:00:51 -0700

    Added right sidebar and updated gitignore

--- frontend/components/right_sidebar/right_sidebar.jsx
+++ frontend/components/right_sidebar/right_sidebar.jsx
@@ -6,7 +6,17 @@ class RightSidebar extends React.Component {
     return (
       <div className="right-sidebar">
         <div className="right-sidebar-container">
-          Right Sidebar
+          <h2 className="right-sidebar-total">
+            Your total balance
+          </h2>
+
+          <div className="right-sidebar-summary">
+            you are owed
+            <br/>
+            <span>
+              $123.45
+            </span>
+          </div>
         </div>
       </div>
     )
