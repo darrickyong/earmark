c1dd3ca8f1a0b5e9e4ae2ef68d49b6a5a9b6648c
darrickyong <darrickyong@gmail.com>
2020-05-05 10:54:08 -0700

    removed unnecessary props

--- frontend/components/expenses/expense_index_item_container.js
+++ frontend/components/expenses/expense_index_item_container.js
@@ -1,16 +1,13 @@
 import { connect } from "react-redux";
 import ExpenseIndexItem from "./expense_index_item";
 import { deleteExpense } from "../../actions/expense_actions";
-import { removeErrors } from "../../actions/session_actions";
 
 const mapStateToProps = (state, ownProps) => ({
-  errors: state.errors.session,
   currentUser: state.entities.users[state.session.id],
 })
 
 const mapDispatchToProps = dispatch => ({
   deleteExpense: expenseId => dispatch(deleteExpense(expenseId)),
-  removeErrors: () => dispatch(removeErrors())
 })
 
 export default connect(
